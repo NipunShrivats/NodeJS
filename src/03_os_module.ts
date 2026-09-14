d000e633671a7337073c4494abcd77a9f76b1179
Nipun Shrivats <nipunrawat16@gmail.com>
2026-09-14 23:31:19 +0530

    changes1

---
+++ src/03_os_module.ts
@@ -0,0 +1,11 @@
+// OS MOdule - operating system
+// OS
+// CPU
+// Memory
+// home/temp dir
+import * as os from "node:os";
+function runOsDemo(): void {
+  console.log("platform:", os.platform());
+}
+
+runOsDemo();
